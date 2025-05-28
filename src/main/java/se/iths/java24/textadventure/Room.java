08c3ba5732b888039da64e479e9dacb6286ddb32
martinez0466 <camilo93.martinez@gmail.com>
2025-05-28 23:38:29 +0200

    feat: Create basic Room class with name and description

---
+++ src/main/java/se/iths/java24/textadventure/Room.java
@@ -0,0 +1,12 @@
+package se.iths.java24.textadventure;
+
+public class Room {
+    String name;
+    String description;
+
+    // Konstruktor
+    public Room(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
